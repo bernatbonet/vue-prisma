d1a18930ae18448c2833f3fd6ac94a1e80dbe529
bernatbonet <bernatbonet@gmail.com>
2017-12-12 17:35:20 +0100

    Aplly filters

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -9,48 +9,56 @@
         </v-card-title>
         <v-form v-model="valid">
           <v-text-field
-            v-bind:label="$t('usercard.labels.username')"
-            v-model="user.username"
+            :label="$t('usercard.labels.username')"
+            :value="user.username | capitalize"
             prepend-icon="account_circle"
             required>
           </v-text-field>
           <v-text-field
-            v-bind:label="$t('usercard.labels.email')"
+            :label="$t('usercard.labels.email')"
             v-model="user.email"
             prepend-icon="email"
             required>
           </v-text-field>
           <v-text-field
-            v-bind:label="$t('usercard.labels.firstName')"
+            :label="$t('usercard.labels.firstName')"
             v-model="user.first_name">
           </v-text-field>
           <v-text-field
-            v-bind:label="$t('usercard.labels.lastName')"
+            :label="$t('usercard.labels.lastName')"
             v-model="user.last_name">
           </v-text-field>
           <v-checkbox
-            v-bind:label="$t('usercard.labels.superUser')"
+            :label="$t('usercard.labels.superUser')"
             v-model="user.is_superuser">
           </v-checkbox>
           <v-checkbox
-            v-bind:label="$t('usercard.labels.active')"
+            :label="$t('usercard.labels.active')"
             v-model="user.is_active">
           </v-checkbox>
           <v-checkbox
-            v-bind:label="$t('usercard.labels.staff')"
+            :label="$t('usercard.labels.staff')"
             v-model="user.is_staff">
           </v-checkbox>
-          <v-flex>
+          <v-text-field
+            :label="$t('usercard.labels.dateJoined')"
+            v-model="user.date_joined"
+            prepend-icon="event"
+            readonly>
+          </v-text-field>
+          <p>{{ user.date_joined | dateFormat }}</p>
+          <!--<v-flex>
             <v-menu>
               <v-text-field
                 slot="activator"
                 v-bind:label="$t('usercard.labels.dateJoined')"
-                v-model="user.date_joined"
+                v-bind:v-model="user.date_joined | dateFormat"
                 prepend-icon="event"
                 readonly>
               </v-text-field>
               <v-date-picker
-                v-model="user.date_joined">
+                v-model="user.date_joined"
+                readonly>
                 <template slot-scope="{ cancelar }">
                   <v-card-actions>
                     <v-spacer></v-spacer>
@@ -68,7 +76,8 @@
                 readonly>
               </v-text-field>
               <v-time-picker
-                v-model="user.date_joined">
+                v-model="user.date_joined"
+                readonly>
                 <template slot-scope="{ cancelar }">
                   <v-card-actions>
                     <v-spacer></v-spacer>
@@ -83,7 +92,7 @@
               <v-text-field
                 slot="activator"
                 v-bind:label="$t('usercard.labels.lastLogin')"
-                v-model="user.last_login"
+                v-bind:v-model="user.last_login"
                 prepend-icon="event"
                 readonly>
               </v-text-field>
@@ -117,8 +126,8 @@
                 </template>
               </v-time-picker>
             </v-menu>
-          </v-flex>
-          <v-btn @click="">{{ $t('usercard.buttons.change_password')}}</v-btn>
+          </v-flex> -->
+          <v-btn @click="change_password">{{ $t('usercard.buttons.change_password')}}</v-btn>
           <v-btn @click="save" :disabled="!valid">{{ $t('usercard.buttons.save')}}</v-btn>
           <v-btn @click="clear">{{ $t('usercard.buttons.cancel')}}</v-btn>
         </v-form>
@@ -128,11 +137,14 @@
 </template>
 
 <script>
+  import moment from 'moment'
+  moment.locale('es')
   export default {
     data () {
       return {
         valid: false,
-        e1: true
+        e1: true,
+        valor: 1225
       }
     },
     computed: {
@@ -140,30 +152,26 @@
         return this.$store.getters['login/getUser']
       }
     },
+    filters: {
+      capitalize: (value) => {
+        if (!value) return ''
+        return value.charAt(0).toUpperCase() + value.slice(1)
+      },
+      uppercase: (value) => {
+        return value.toUpperCase()
+      },
+      dateFormat: (value) => {
+        return moment(value).format('L')
+      },
+      euros: (value) => {
+        return value.toFixed(2) + ' €'
+      }
+    },
     methods: {
-      formatDate (date) {
-        if (date) {
-          const [day, month, year] = date.split('/')
-          return `${day}/${month}/${year}`
-        } else {
-          return null
-        }
-      },
-      parseDate (date) {
-        if (!date) {
-          return null
-        }
-
-        const [month, day, year] = date.split('/')
-        return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`
-      },
-      guardarFechaAlta () {
+      change_password () {
         return null
       },
-      cancelarFechaAlta () {
-        return null
-      },
-      submit () {
+      save () {
       },
       clear () {
         this.$refs.form.reset()
